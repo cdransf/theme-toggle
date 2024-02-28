06f6f1421daad606df3f0307ae7c1a304ef39696
Cory Dransfeldt <hi@coryd.dev>
2024-02-28 13:14:13 -0800

    fix: bad reference

--- theme-load.js
+++ theme-load.js
@@ -1,6 +1,6 @@
 (() => {
   const currentTheme = sessionStorage?.getItem('theme');
-  if (!currentTheme) sessionStorage?.setItem('theme', (this.prefersDarkScheme ? 'dark' : 'light'))
+  if (!currentTheme) sessionStorage?.setItem('theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
   if (currentTheme === 'dark') {
     document.body.classList.add('theme__dark')
   } else if (currentTheme === 'light') {
