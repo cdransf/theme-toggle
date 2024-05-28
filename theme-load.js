4372e09833bb8ea780d6f698a079c36f0e877c16
Cory Dransfeldt <hi@coryd.dev>
2024-05-28 08:57:35 -0700

    chore: simplify logic

--- theme-load.js
+++ theme-load.js
@@ -1,15 +1,7 @@
 (() => {
-  const currentTheme = sessionStorage?.getItem('theme');
-  const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const root = document.getElementsByTagName('html')[0];
+  const currentTheme = sessionStorage?.getItem('theme')
+  const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+  const root = document.getElementsByTagName('html')[0]
   if (!currentTheme) sessionStorage?.setItem('theme', (prefersDarkScheme ? 'dark' : 'light'))
-  if (currentTheme === 'dark') {
-    root.classList.add('theme__dark')
-  } else if (currentTheme === 'light') {
-    root.classList.add('theme__light')
-  } else if (prefersDarkScheme) {
-    root.classList.add('theme__dark')
-  } else if (!prefersDarkScheme) {
-    root.classList.add('theme__light')
-  }
-})();
+  root.classList.add(`theme__${currentTheme || (prefersDarkScheme ? 'dark' : 'light')}`)
+})()
--- theme-toggle.js
+++ theme-toggle.js
@@ -1,42 +1,37 @@
 class ThemeToggle extends HTMLElement {
-  static tagName = 'theme-toggle';
+  static tagName = 'theme-toggle'
 
-  static register(tagName, registry) {
-    if(!registry && ('customElements' in globalThis)) {
-      registry = globalThis.customElements;
-    }
-    registry?.define(tagName || this.tagName, this);
+  static register(tagName = this.tagName, registry = globalThis.customElements) {
+    registry.define(tagName, this)
   }
 
   connectedCallback() {
-    if (this.shadowRoot) return;
-
-    let shadowroot = this.attachShadow({ mode: 'open' })
-    let slot = document.createElement('slot')
-    shadowroot.appendChild(slot)
-
-    this.root = document.getElementsByTagName('html')[0];
+    if (this.shadowRoot) return
+    this.attachShadow({ mode: 'open' }).appendChild(document.createElement('slot'))
+    this.root = document.documentElement
     this.button = this.querySelector('button')
     this.prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches
-    this.currentTheme = sessionStorage?.getItem('theme')
-    this.theme
-    this.setTheme = () => {
-      if (!this.currentTheme) sessionStorage?.setItem('theme', (this.prefersDarkScheme ? 'dark' : 'light'))
+    this.currentTheme = sessionStorage.getItem('theme')
+    this.setTheme()
+    this.button.addEventListener('click', () => this.toggleTheme())
+  }
 
-      if (this.prefersDarkScheme) {
-        this.theme = this.root.classList.contains('theme__light') ? 'light' : 'dark'
-      } else {
-        this.theme = this.root.classList.contains('theme__dark') ? 'dark' : 'light'
-      }
-      sessionStorage?.setItem('theme', this.theme)
+  setTheme() {
+    if (!this.currentTheme) {
+      this.currentTheme = this.prefersDarkScheme ? 'dark' : 'light'
+      sessionStorage.setItem('theme', this.currentTheme)
     }
-    this.setTheme();
-    this.button.addEventListener('click', () => {
-      this.root.classList.toggle('theme__light')
-      this.root.classList.toggle('theme__dark')
-      this.setTheme()
-    })
+
+    this.theme = this.root.classList.contains('theme__light') ? 'light' : 'dark'
+
+    if (!this.root.classList.contains(`theme__${this.theme}`)) this.root.classList.add(`theme__${this.theme}`)
+  }
+
+  toggleTheme() {
+    this.root.classList.toggle('theme__light')
+    this.root.classList.toggle('theme__dark')
+    this.setTheme()
   }
 }
 
-ThemeToggle.register();
+ThemeToggle.register()
