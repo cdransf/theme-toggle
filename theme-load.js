d13cdf55fd8489e7ecf22dd5b8a56710be0614fa
Cory Dransfeldt <hi@coryd.dev>
2024-09-02 15:12:58 -0700

    feat: replace classes with data attributes

--- theme-load.js
+++ theme-load.js
@@ -1,7 +1,8 @@
 (() => {
   const currentTheme = sessionStorage?.getItem('theme')
   const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches
-  const root = document.getElementsByTagName('html')[0]
-  if (!currentTheme) sessionStorage?.setItem('theme', (prefersDarkScheme ? 'dark' : 'light'))
-  root.classList.add(`theme__${currentTheme || (prefersDarkScheme ? 'dark' : 'light')}`)
+  const root = document.documentElement
+  const themeToSet = currentTheme || (prefersDarkScheme ? 'dark' : 'light')
+  if (!currentTheme) sessionStorage?.setItem('theme', themeToSet)
+  root.setAttribute('data-theme', themeToSet)
 })()
--- theme-toggle.js
+++ theme-toggle.js
@@ -10,32 +10,35 @@ class ThemeToggle extends HTMLElement {
     this.attachShadow({ mode: 'open' }).appendChild(document.createElement('slot'))
     this.root = document.documentElement
     this.button = this.querySelector('button')
-    this.prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+    this.prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)')
     this.currentTheme = sessionStorage.getItem('theme')
+
     this.setTheme()
+
     this.button.addEventListener('click', () => this.toggleTheme())
+    this.prefersDarkScheme.addEventListener('change', (event) => this.onPreferredColorSchemeChange(event))
   }
 
   setTheme() {
     if (!this.currentTheme) {
-      this.currentTheme = this.prefersDarkScheme ? 'dark' : 'light'
+      this.currentTheme = this.prefersDarkScheme.matches ? 'dark' : 'light'
     }
 
     this.theme = this.currentTheme
-
-    if (this.theme === 'dark') {
-      this.root.classList.add('theme__dark')
-      this.root.classList.remove('theme__light')
-    } else {
-      this.root.classList.add('theme__light')
-      this.root.classList.remove('theme__dark')
-    }
+    this.root.setAttribute('data-theme', this.theme)
   }
 
   toggleTheme() {
     this.currentTheme = this.currentTheme === 'dark' ? 'light' : 'dark'
     sessionStorage.setItem('theme', this.currentTheme)
     this.setTheme()
+  }
+
+  onPreferredColorSchemeChange(event) {
+    if (!sessionStorage.getItem('theme')) {
+      this.currentTheme = event.matches ? 'dark' : 'light'
+      this.setTheme()
+    }
   }
 }
 
