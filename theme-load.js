578fa30ac2f07b30b2fc40e33b1ade1c9cf55b97
Cory Dransfeldt <hi@coryd.dev>
2024-10-30 17:01:52 -0700

    chore: improve initial load

--- theme-load.js
+++ theme-load.js
@@ -6,6 +6,9 @@
 
   const currentTheme = storageType.getItem("theme") || "auto";
   const metaColorScheme = document.querySelector('meta[name="color-scheme"]');
+  const lightIcon = document.querySelector(".light");
+  const darkIcon = document.querySelector(".dark");
+  const autoIcon = document.querySelector(".auto");
 
   if (currentTheme === "auto") {
     metaColorScheme.setAttribute("content", "light dark");
@@ -13,5 +16,17 @@
   } else {
     metaColorScheme.setAttribute("content", currentTheme);
     document.documentElement.style.colorScheme = currentTheme;
+  }
+
+  lightIcon.style.display = "none";
+  darkIcon.style.display = "none";
+  autoIcon.style.display = "none";
+
+  if (currentTheme === "light") {
+    lightIcon.style.display = "inline";
+  } else if (currentTheme === "dark") {
+    darkIcon.style.display = "inline";
+  } else {
+    autoIcon.style.display = "inline";
   }
 })();
